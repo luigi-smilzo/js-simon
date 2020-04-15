ab7432763590ef333aafd38ad473c66b0e79417f
Luigi <luigismilzo@live.com>
2020-04-15 16:31:16 +0200

    Validation added

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@
  * Assicuratevi di saper fare correttamente l’ esercizio con output base (alert() , prompt() , console.log() )
  **************************************************************************************************************************/
 
+// REFERENCES
 var rndNum = [];
 var playerInputs = [];
 var matching = [];
 var time = 30;
 
+// SETUP
 for ( var i = 0; rndNum.length < 5; i++) {
     var item = Math.floor( Math.random() * 1000) + 1;
     if ( !rndNum.includes(item)) {
@@ -22,6 +24,7 @@ for ( var i = 0; rndNum.length < 5; i++) {
 
 alert(rndNum);
 
+// COUNTDOWN
 var cntDwn = setInterval(function() {
     console.log(time);
     if (time == 0) {
@@ -31,21 +34,33 @@ var cntDwn = setInterval(function() {
     }
 }, 1000);
 
+// PLAYER IS WRITING NUMBERS...
 setTimeout(function () {
+
+    // Player inputs
     for ( var i = 0; i < 5; i++) {
-        var input = parseInt(prompt('Inserisci un numero'));
+        var input = parseInt(prompt('Inserisci un numero da 1 a 1000'));
+
+        // Validation
+        while ( (input < 1 || input > 1000) || isNaN(input) ) {
+            input = parseInt(prompt('Per favore, inserisci un numero da 1 a 1000'));
+        }
+
         playerInputs.push(input);
     }
+
+    // Checking results...
     for ( var i = 0; i < rndNum.length; i++) {
         var toMatch = playerInputs[i];
         if ( rndNum.includes(toMatch) ) {
             matching.push(toMatch);
         }
     }
+
+    // Results
     console.log('Numeri da ricordare:', rndNum);
     console.log('Punteggio:', matching.length);
     console.log('Numeri indovinati:', matching);
 }, 31000);
-
 
 
